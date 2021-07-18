f18202d2f8f40c9a391ea0d0fc4360ea925c10b6
sivasurya16 <www.siva.com91@gmail.com>
2021-07-18 18:10:07 +0530

    some more changes

--- scripts/scroll_effect.js
+++ scripts/scroll_effect.js
@@ -2,9 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		document.getElementById("header").style.fontSize = "40px";
+		document.getElementById("header").style.fontSize = "3vw";
+		document.getElementById("header").setAttribute('align','center')
 	} 
 	else {
-		document.getElementById("header").style.fontSize = "100px";
+		document.getElementById("header").style.fontSize = "6vw";
+		document.getElementById("header").setAttribute('align','center')
 	}
 }
